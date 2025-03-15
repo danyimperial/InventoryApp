54dac9b85af363e7467cccb1f652c843750176f3
danyimperial <danyelalyssa.imperial@letran.edu.ph>
2025-03-15 11:25:45 +0800

    Added Background on HomePage

--- src/screens/HomePageScreen.js
+++ src/screens/HomePageScreen.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {
     Text,
+    ImageBackground,
     View,
     Image,
     TouchableOpacity
 } from "react-native";
 import { HomePageStyle } from '../styles/HomePageStyle';
+
 const HomePageScreen = ({ navigation }) => {
     const handleLogin = () => {
         navigation.navigate('LoginScreen');
@@ -13,24 +15,34 @@ const HomePageScreen = ({ navigation }) => {
     const handleRegister = () => {
         navigation.navigate('RegisterScreen');
     };
+
     return (
-        <View style={HomePageStyle.View}>
-            <Image
-                style={HomePageStyle.Logo}
-                source={require('../assets/logo.webp')} />
-            <TouchableOpacity
-                style={HomePageStyle.RegisterButton}>
-                <Text onPress={handleRegister}>
-                    REGISTER
-                </Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-                style={HomePageStyle.LoginButton}>
-                <Text onPress={handleLogin}>
-                    LOGIN
-                </Text>
-            </TouchableOpacity>
-        </View>
+        <ImageBackground
+            source={require('../assets/background.jpeg')}
+            style={HomePageStyle.Background}
+        >
+            <View style={HomePageStyle.Background}>
+                <Image
+                    style={HomePageStyle.Logo}
+                    source={require('../assets/logo.webp')} />
+        
+                {/* Register Button */}
+                <TouchableOpacity
+                    style={HomePageStyle.RegisterButton}>
+                    <Text onPress={handleRegister}>
+                        REGISTER
+                    </Text>
+                </TouchableOpacity>
+
+                {/* Login Button */}
+                <TouchableOpacity
+                    style={HomePageStyle.LoginButton}>
+                    <Text onPress={handleLogin}>
+                        LOGIN
+                    </Text>
+                </TouchableOpacity>
+            </View>
+        </ImageBackground>
     )
 }
 
--- src/styles/HomePageStyle.js
+++ src/styles/HomePageStyle.js
@@ -1,35 +1,37 @@
 import { StyleSheet } from "react-native";
 
 export const HomePageStyle = StyleSheet.create({
-    View: {
+    Background: {
         flex: 1, 
-        justifyContent: 'center', 
+        justifyContent: 'center',
         alignItems: 'center',
-        // backgroundImage: "url('../assets/background.jpeg')",
+        width: '100%'
     },
     Logo: {
         width: 356,
         height: 300,
-        margin: 10
-
+        margin: 10,
     },
     RegisterButton: {
         borderWidth: 1,
         borderColor: '1D1616',
+        width: '40%',
         paddingTop: 10,
         paddingBottom: 10,
-        paddingLeft: 49,
-        paddingRight: 49,
+        paddingLeft: 20,
+        paddingRight: 20,
         margin: 5,
+        alignItems: 'center'
     },
     LoginButton: {
         borderWidth: 1,
         borderColor: '1D1616',
+        width: '40%',
         paddingTop: 10,
         paddingBottom: 10,
-        paddingLeft: 60,
-        paddingRight: 60,
+        paddingLeft: 20,
+        paddingRight: 20,
         margin: 5,
-        alignSelf: 'center'
+        alignItems: 'center'
     }
 })
